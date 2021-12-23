83662ad59a5163f78e669641a8517031ddf8bf44
youssef-rabbani-1 <93159700+youssef-rabbani-1@users.noreply.github.com>
2021-12-23 06:39:51 +0200

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,6 @@ export async function getStaticProps({ locale }) {
   const [categories, homepage] = await Promise.all([
     fetchAPI("/categories"),
     fetchAPI("/homepage"),
-
   ])
 
   return {
